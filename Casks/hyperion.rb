d8da24d55ea746c2b331d68ebb77b2d3763df145
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2025-05-31 10:57:22 +0200

    typo

--- Casks/hyperion.rb
+++ Casks/hyperion.rb
@@ -1,6 +1,6 @@
 cask "hyperion" do
   arch arm: "x64", intel: "x64"
-  version: "2.0.16"
+  version "2.0.16"
   sha256 arm: "9cc530d5772570bb41ad8a3e2b553f211f373a616a731189192910d33effa6fc", intel: "9cc530d5772570bb41ad8a3e2b553f211f373a616a731189192910d33effa6fc"
   url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-#{arch}.dmg", verified: "github.com/hyperion-project/hyperion.ng/"
   name "Hyperion"
