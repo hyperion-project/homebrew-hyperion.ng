97974b6dd2601c699fdd7a1ed1e5d093078df0e3
Paulchen-Panther <16664240+Paulchen-Panther@users.noreply.github.com>
2025-07-03 17:54:50 +0200

    update hyperion cask to 2.1.1

--- Casks/hyperion.rb
+++ Casks/hyperion.rb
@@ -1,7 +1,16 @@
 cask "hyperion" do
-  arch arm: "x64", intel: "x64"
-  version "2.0.16"
-  sha256 arm: "9cc530d5772570bb41ad8a3e2b553f211f373a616a731189192910d33effa6fc", intel: "9cc530d5772570bb41ad8a3e2b553f211f373a616a731189192910d33effa6fc"
+  arch arm: "arm64", intel: "x86_64"
+  version "2.1.1"
+  sha256 arm: "a922247fcdd77b6cc2a4ca8600e2ec1ce89d706b6dcdd658f460534a1daa23d9", intel: "424e5d2223de3f6600efcbf8c08621f9351029aa5adce33d9332a0af7a4bf5c7"
+
+  on_arm do
+    depends_on macos: ">= :sonoma"
+  end
+  on_intel do
+    depends_on macos: ">= :ventura"
+  end
+
+
   url "https://github.com/hyperion-project/hyperion.ng/releases/download/#{version}/Hyperion-#{version}-macOS-#{arch}.dmg", verified: "github.com/hyperion-project/hyperion.ng/"
   name "Hyperion"
   desc "The successor to Hyperion aka Hyperion Next Generation"
@@ -13,7 +22,6 @@ cask "hyperion" do
   end
 
   conflicts_with formula: "hyperion"
-  depends_on macos: ">= :big_sur"
 
   app "Hyperion.app"
   uninstall quit: "com.hyperion-project.Hyperion"
